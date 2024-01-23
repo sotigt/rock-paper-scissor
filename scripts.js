5b407c7e9bebafe65e005842be2db6e61d074ddd
sotigt <78357279+sotigt@users.noreply.github.com>
2024-01-23 15:58:53 +0100

    Create function for the computer's choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+//****  ROCK PAPER SCISSORS
+// Fuction to randomize computer choice 
+// Function to compare user choice to computer choice 
+// Depending on result show different text
+
+function getComputerChoice () {
+
+    const options = ["rock", "paper", "scissors"];
+
+    let randomIndex = Math.floor(Math.random() * options.length);
+    let randomOption = options[randomIndex];
+
+    return randomOption;
+}
